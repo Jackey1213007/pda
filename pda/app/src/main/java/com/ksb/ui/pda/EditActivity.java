e56e29fe4541d00c41ebff06497c9e7202ab41e5
Jackey1213007 <wsm964520547@163.com>
2018-10-23 14:24:17 +0800

    add00000000
    6

---
+++ pda/app/src/main/java/com/ksb/ui/pda/EditActivity.java
@@ -0,0 +1,20 @@
+package com.ksb.ui.pda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+	
+	login(){
+		0000000
+		
+		55555555
+		66666666
+	}
+}
